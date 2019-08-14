d7a32397873b0dd91e877eecb5e81664c1101fac
ZorRro <debadattameher747@gmail.com>
2019-08-15 05:02:26 +0530

    mend

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -10,122 +10,121 @@ const UserRouter = express.Router();
 // Routes
 
 UserRouter.post("/update", authUtil.verifyToken, (req, res) => {
-    const userId = req.body.userId;
-    User.findById(userId)
-        .then(user => {
-            console.log(`User Retrieved : ${user}`);
-            if (user.id === userId) {
-                // Legitimate User
-                if (user.username !== userUpdateDetails.username) {
-                    // User has requested to change username. Disallow.
-                    res.json(400).json({ message: "Username cannot be changed." });
-                }
-                const updatedUser = new User();
-                updatedUser.username = user.username;
-                updatedUser.password = userUpdateDetails.password || user.password;
-                updatedUser.email = userUpdateDetails.email || user.email;
-                updatedUser.name = userUpdateDetails.name || user.name;
-                return updatedUser;
-            } else {
-                res.status(404).json({ message: "User is not authenticated." });
-            }
-        })
-        .then(updatedUser => {
-            console.log(`res : ${updatedUser}`);
-            updatedUser.save().then(result => {
-                console.log("User Updated");
-                res.status(200).json(result);
-            });
-        })
-        .catch(err => {
-            console.error(err);
-            res.status(400).send({ message: "Unable to update user." });
-        });
+  const userId = req.body.userId;
+  User.findById(userId)
+    .then(user => {
+      console.log(`User Retrieved : ${user}`);
+      if (user.id === userId) {
+        // Legitimate User
+        if (user.username !== userUpdateDetails.username) {
+          // User has requested to change username. Disallow.
+          res.json(400).json({ message: "Username cannot be changed." });
+        }
+        const updatedUser = new User();
+        updatedUser.username = user.username;
+        updatedUser.password = userUpdateDetails.password || user.password;
+        updatedUser.email = userUpdateDetails.email || user.email;
+        updatedUser.name = userUpdateDetails.name || user.name;
+        return updatedUser;
+      } else {
+        res.status(404).json({ message: "User is not authenticated." });
+      }
+    })
+    .then(updatedUser => {
+      console.log(`res : ${updatedUser}`);
+      updatedUser.save().then(result => {
+        console.log("User Updated");
+        res.status(200).json(result);
+      });
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(400).send({ message: "Unable to update user." });
+    });
 });
 
 UserRouter.get("/delete", authUtil.verifyToken, (req, res) => {
-    const userId = req.body.userId;
-    User.findByIdAndRemove(userId)
-        .then(result => {
-            if (!result) {
-                // The token was valid. So, the userId was set in the request body.
-                // But user is already removed from the application's system.
-                res.status(404).json({ message: "User is not registered." });
-            }
-            console.log(`user removed. ${result}`);
-            res.status(200).json({ message: "User is removed from the system." });
-        })
-        .catch(err => {
-            console.error(`error occurred while removing user. ${err}`);
-            res.status(500).json({ message: "Error occurred while removing user." });
-        });
+  const userId = req.body.userId;
+  User.findByIdAndRemove(userId)
+    .then(result => {
+      if (!result) {
+        // The token was valid. So, the userId was set in the request body.
+        // But user is already removed from the application's system.
+        res.status(404).json({ message: "User is not registered." });
+      }
+      console.log(`user removed. ${result}`);
+      res.status(200).json({ message: "User is removed from the system." });
+    })
+    .catch(err => {
+      console.error(`error occurred while removing user. ${err}`);
+      res.status(500).json({ message: "Error occurred while removing user." });
+    });
 });
 
-UserRouter.get("/", authUtil.verifyToken, (req, res) => {
-    const userId = req.body.userId;
-    User.findById(userId)
-        .populate("note.id")
-        .then(user => {
-            if (!user) {
-                res.status(404).json({ message: "User is not registered" });
-            }
-            notesPromiseMap = user.notes.map(noteId => Note.findById(noteId))
-            Promise.all(notesPromiseMap)
-                .then(notesContent => {
-                    const userData = {
-                        username: user.username,
-                        name: user.name,
-                        notes: notesContent,
-                        email: user.email
-                    }
-                    res.status(200).json({ user: userData });
-                })
-        })
-        .catch(err => {
-            console.error(`Error occurred : ${err}`);
-            res.status(500).json({ message: "Internal Server Error" });
-        });
+UserRouter.get("/:id", authUtil.verifyToken, (req, res) => {
+  const userId = req.params.id;
+  User.findById(userId)
+    .populate("note.id")
+    .then(user => {
+      if (!user) {
+        res.status(404).json({ message: "User is not registered" });
+      }
+      notesPromiseMap = user.notes.map(noteId => Note.findById(noteId));
+      Promise.all(notesPromiseMap).then(notesContent => {
+        const userData = {
+          username: user.username,
+          name: user.name,
+          notes: notesContent,
+          email: user.email
+        };
+        res.status(200).json({ user: userData });
+      });
+    })
+    .catch(err => {
+      console.error(`Error occurred : ${err}`);
+      res.status(500).json({ message: "Internal Server Error" });
+    });
 });
 
 UserRouter.post("/notes/add", authUtil.verifyToken, (req, res) => {
-    const userId = req.body.userId;
-    let tempUser = null,
-        newNote = null
-    tempNoteSaveResult = null;
+  const userId = req.body.userId;
+  let tempUser = null,
+    newNote = null;
+  tempNoteSaveResult = null;
 
-    User.findById(userId)
-        .populate("notes.id")
-        // .execPopulate()
-        .then(user => {
-            if (!user) {
-                res.status(404).json({ message: "User is not registered." });
-            }
-            let reqNote = req.body.note;
-            reqNote.author = userId;
-            newNote = new Note(reqNote);
-            tempUser = user;
-            return newNote.save();
-        })
-        .then(noteSaveResult => {
-            if (!noteSaveResult) {
-                res
-                    .status(500)
-                    .json({ message: "Unable to save a new note at the moment." });
-            }
-            tempUser.notes.push(newNote.id);
-            tempNoteSaveResult = noteSaveResult;
-            return tempUser.save();
-        })
-        .then(userSaveResult => {
-            if (!userSaveResult) {
-                res.status(500).json({ message: "Internal Server Error." });
-            }
-            res.status(200).json({ note: tempNoteSaveResult });
-        })
-        .catch(err => {
-            console.error(`error occurred. ${err}`);
-            res.status(500).json({ message: err });
-        });
+  User.findById(userId)
+    .populate("notes.id")
+    // .execPopulate()
+    .then(user => {
+      if (!user) {
+        res.status(404).json({ message: "User is not registered." });
+      }
+      let reqNote = req.body.note;
+      reqNote.author = userId;
+      newNote = new Note(reqNote);
+      tempUser = user;
+      return newNote.save();
+    })
+    .then(noteSaveResult => {
+      if (!noteSaveResult) {
+        res
+          .status(500)
+          .json({ message: "Unable to save a new note at the moment." });
+      }
+      tempUser.notes.push(newNote.id);
+      tempNoteSaveResult = noteSaveResult;
+      return tempUser.save();
+    })
+    .then(userSaveResult => {
+      if (!userSaveResult) {
+        res.status(500).json({ message: "Internal Server Error." });
+      }
+      res.status(200).json({ note: tempNoteSaveResult });
+    })
+    .catch(err => {
+      console.error(`error occurred. ${err}`);
+      res.status(500).json({ message: err });
+    });
 });
 
 module.exports = UserRouter;
--- util/auth.util.js
+++ util/auth.util.js
@@ -3,59 +3,57 @@ const bcrypt = require("bcrypt");
 const secret = process.env.SECRET;
 
 module.exports.validateSignupUserData = function(userData) {
-    if (
-        userData.username &&
-        userData.password &&
-        userData.name &&
-        userData.email
-    ) {
-        return true;
-    }
-    return false;
+  if (
+    userData.username &&
+    userData.password &&
+    userData.name &&
+    userData.email
+  ) {
+    return true;
+  }
+  return false;
 };
 
 module.exports.hashContent = function(content) {
-    console.log("In Hashing function.");
-    return bcrypt.hash(content, 12);
+  console.log("In Hashing function.");
+  return bcrypt.hash(content, 12);
 };
 
 module.exports.comparePasswords = function(requestPassword, user) {
-    return bcrypt.compare(requestPassword, user.password)
-        .then(match => {
-            if (match) {
-                const payload = user.id;
-                const token = jwt.sign(payload, secret);
-                const data = {
-                    token: token,
-                    id: payload,
-                    name: user.name,
-                    notes: user.notes,
-                    email: user.email
-                };
-                // this data will be sent in response
-                return data;
-            } else {
-                throw new Error("Username/Password did not match.");
-            }
-        });
+  return bcrypt.compare(requestPassword, user.password).then(match => {
+    if (match) {
+      const payload = user.id;
+      const token = jwt.sign(payload, secret);
+      const data = {
+        token: token,
+        id: payload,
+        name: user.name,
+        notes: user.notes,
+        email: user.email
+      };
+      // this data will be sent in response
+      return data;
+    } else {
+      throw new Error("Username/Password did not match.");
+    }
+  });
 };
 
-
 module.exports.verifyToken = function(req, res, next) {
-    if (!req.headers.authorization) {
-        res.status(401).json({ message: 'Unauthorized request' });
-    }
-    const token = req.headers.authorization.split(' ')[1]
-    if (!token) {
-        res.status(401).json({ message: 'Unauthorized request' });
-    }
-    const payload = jwt.verify(token, secret)
-    if (!payload) {
-        res.status(401).json({ message: 'Unauthorized request' });
-    }
-    console.log('User is authorized.')
-        // User is validated. Set the userID in request for next middlewares
-        // userId will be available for the other middlewares
-    req.body.userId = payload
-    next()
-}
+  if (!req.headers.authorization) {
+    return res.status(401).json({ message: "Unauthorized request" });
+  }
+  const token = req.headers.authorization.split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized request" });
+  }
+  const payload = jwt.verify(token, secret);
+  if (!payload) {
+    return res.status(401).json({ message: "Unauthorized request" });
+  }
+  console.log("User is authorized.");
+  // User is validated. Set the userID in request for next middlewares
+  // userId will be available for the other middlewares
+  req.body.userId = payload;
+  next();
+};
